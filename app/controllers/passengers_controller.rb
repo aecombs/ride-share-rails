ef54837f6c68040921e7d838e3e9f998ba1d963c
Alicia Combs <alicia.combs.92@gmail.com>
2020-05-08 10:51:01 -0700

    Added functionality to passengers#create and passengers#new, edited the show.html.erb to have logic to only show what trips if there are trips and charges if there are charges

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -4,16 +4,24 @@ class PassengersController < ApplicationController
   end
 
   def new
-      
+    @passenger = Passenger.new
   end
 
   def create
+    @passenger = Passenger.new(passenger_params)
+    if @passenger.save
+      redirect_to passenger_path(@passenger.id)
+      return
+    else
+      render :new, :bad_request
+      return
+    end
   end
 
   def show
     @passenger = Passenger.find_by(id: params[:id].to_i)
     if @passenger.nil?
-      head :not_found
+      redirect_to passengers_path
       return
     end
   end
@@ -42,6 +50,14 @@ class PassengersController < ApplicationController
   end
 
   def destroy
+    @passenger = Passenger.find_by(id: params[:id])
+    if @passenger.nil?
+      head :not_found
+      return
+    end
+
+    @passenger.destroy
+    redirect_to passengers_path
   end
 
   private
