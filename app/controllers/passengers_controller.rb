49345ef9cd468dac16f292eae4f8b54966e45e44
Alicia Combs <alicia.combs.92@gmail.com>
2020-05-08 10:38:29 -0700

    Added passenger_params method, form partial, and functionality to edit.html.erb and its controller action

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,29 +1,52 @@
 class PassengersController < ApplicationController
-    def index
-      @passengers = Passenger.all
+  def index
+    @passengers = Passenger.all
+  end
+
+  def new
+      
+  end
+
+  def create
+  end
+
+  def show
+    @passenger = Passenger.find_by(id: params[:id].to_i)
+    if @passenger.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def edit
+    @passenger = Passenger.find_by(id: params[:id].to_i)
+    if @passenger.nil?
+      head :not_found
+      return
     end
 
-    def new
-        
-    end
+  end
 
-    def create
+  def update
+    @passenger = Passenger.find_by(id: params[:id].to_i)
+    if @passenger.nil?
+      head :not_found
+      return
+    elsif @passenger.update(passenger_params)
+      redirect_to passenger_path(@passenger.id)
+      return
+    else
+      render :edit
+      return
     end
+  end
 
-    def show
-      @passenger = Passenger.find_by(id: params[:id].to_i)
-      if @passenger.nil?
-        head :not_found
-        return
-      end
-    end
+  def destroy
+  end
 
-    def edit
-    end
+  private
 
-    def update
-    end
-
-    def destroy
-    end
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_num)
+  end
 end
