d1f70125e04cf47f2143b0ef92ec5fa4b7b2086d
Maryam Heshmati <mairheshmati@gmail.com>
2020-05-08 10:48:43 -0700

    Rendered the list of trips for each driver, wrote an algorathim in the model to calculate the total earning and average rating for each driver.

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,19 @@
 class Driver < ApplicationRecord
     has_many :trips
+
+    def total_earning
+        total = 0 
+        self.trips.each do |trip|
+            total += trip.cost
+        end
+        return total
+    end
+
+    def average_rating
+        total_rating = 0 
+        self.trips.each do |trip|
+            total_rating += trip.rating
+        end
+        return total_rating / self.trips.length
+    end
 end
