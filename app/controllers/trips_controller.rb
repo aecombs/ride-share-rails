224e2d85afe4ff7bac76736136b5150e49a5a5a4
Maryam Heshmati <mairheshmati@gmail.com>
2020-05-11 10:33:43 -0700

    nested routes for trips and created a index action for trips and a html.erb file to render the trips for passengers and drivers.

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,14 @@
 class TripsController < ApplicationController
+  def index
+    if params[:passenger_id]
+      passenger = Passenger.find_by(id: params[:passenger_id])
+      @trips = passenger.trips
+    elsif params[:driver_id]
+      driver = Driver.find_by(id: params[:driver_id])
+      @trips = driver.trips
+    else
+      @trips = Trip.all
+    end
+  end
+  
 end
